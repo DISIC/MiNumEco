53816ab48a8e3809e32ccb8aa423edc1b239fa22
Richard <supertanuki@gmail.com>
2021-04-30 15:00:25 +0200

    Correction tag titre page suivi d'audience

--- js/mesure-audience.js
+++ js/mesure-audience.js
@@ -5,7 +5,7 @@ var tag = new ATInternet.Tracker.Tag({ sendHitWhenOptOut: false });
         
 if (!isOptout()) {
     tag.privacy.setVisitorMode('cnil', 'exempt');
-    tag.page.set({ name: "{{ seoTitle or title or metadata.title }}" });
+    tag.page.set({ name: document.title });
     tag.dispatch();
 }
 
